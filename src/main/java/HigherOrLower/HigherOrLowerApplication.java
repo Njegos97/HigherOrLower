ac5aaee69f3df1291d8d92dd6ccff34ea7682c95
njegoss997@gmail.com <njegoss997@gmail.com>
2019-09-17 09:38:33 +0200

    first commit

---
+++ src/main/java/HigherOrLower/HigherOrLowerApplication.java
@@ -0,0 +1,13 @@
+package HigherOrLower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HigherOrLowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HigherOrLowerApplication.class, args);
+	}
+
+}
